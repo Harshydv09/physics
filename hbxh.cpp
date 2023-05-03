d89e1a5e25a080281f7e8f5a4687c8e3fc9a3bab
Harshydv09 <yadavharsh090703@gmail.com>
2023-05-03 22:32:31 +0530

    loop

--- hbxh.cpp
+++ hbxh.cpp
@@ -1,19 +1,48 @@
-#include <cmath>
-#include <cstdio>
-#include <vector>
 #include <iostream>
-#include <algorithm>
+#include <cstdio>
 using namespace std;
 
-int solveMeFirst(int a, int b) {
-  return a+b;
-}
-
 int main() {
-  int num1, num2;
-  int sum;
-  cin>>num1>>num2;
-  sum = solveMeFirst(num1,num2);
-  cout<<sum;
-  return 0;
+    // Complete the code.
+ int n,m,i;
+ 
+ cin>>i;
+ cin>>m;
+ for(n=i;n<=m;n++){
+     if(n==1){
+        cout<<"one"<<endl;
+    }
+    else if(n==2){
+        cout<<"two"<<endl;
+    }
+    else if(n==3){
+        cout<<"three"<<endl;
+    }
+    else if(n==4){
+        cout<<"four"<<endl;
+    }
+    else if (n==5){
+        cout<<"five"<<endl;
+    }
+    else  if(n==6){
+        cout<<"six"<<endl;
+    }
+    else if (n==7){
+        cout<<"seven"<<endl;
+    }
+    else if (n==8){
+        cout<<"eight"<<endl;
+    }
+    else if (n==9){
+        cout<<"nine"<<endl;
+    }
+ 
+ else if (n>9&&n%2==0){
+        cout<<"even"<<endl;
+    }
+    else  {
+        cout<<"odd"<<endl;
+    }
+ }
+    return 0;
 }
